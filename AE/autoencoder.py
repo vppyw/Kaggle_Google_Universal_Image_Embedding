793a15b1344f32054d0a9e784e24a44312a38f32
vppyw <vincent01pao@gmail.com>
2022-08-18 00:47:22 +0800

    Add basic autoencoder

---
+++ AE/autoencoder.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models, transforms
+
+class Encoder(nn.Module):
+    def __init__(self, out_dim=64):
+        super().__init__()
+        self.cnn = models.resnet50()
+        self.linear = nn.Sequential(
+                    nn.BatchNorm1d(1000),
+                    nn.ReLU(),
+                    nn.Linear(1000, 64),
+                   )
+
+    def forward(self, x):
+        x = self.cnn(x)
+        x = self.linear(x)
+        return x
+
+class Decoder(nn.Module):
+    def __init__(self, out_dim=(224, 224)):
+        super().__init__()
+        self.dconv = nn.Sequential(
+            nn.ConvTranspose2d(16, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.ConvTranspose2d(8, 8, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d(output_size=(112, 112)),
+            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),
+        ) 
+
+    def forward(self, x):
+        x = x.reshape(-1, 16, 2, 2)
+        return self.dconv(x)
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x, latent=True):
+        if latent:
+            x = transforms.functional.resize(x, size=[224, 224])
+            x = x / 255.0
+            x = transforms.functional.normalize(x, 
+                                            mean=[0.485, 0.456, 0.406], 
+                                            std=[0.229, 0.224, 0.225])
+            x = self.encoder(x)
+            return x
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
