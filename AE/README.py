0ecce498066f34479cce9c4b5c4715e549ddbc09
vppyw <vincent01pao@gmail.com>
2022-08-19 17:19:33 +0800

    Update AE/README.md

--- AE/README.py
+++ AE/README.py
@@ -1,5 +1,12 @@
+## Description
+
 Use the encoder of an auto-encoder to do image embedding.
 
-Training on CIFAR-10
+Training auto-encoder on CIFAR-10
 
 kaggle score: 0.025
+
+## log:
+
+20220819:
+    It seems like this is not a good method.
